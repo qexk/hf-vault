a4a878a0443c099d3a7588a69c384564b26301e1
Alexandre Szymocha <alexandre@szymocha.com>
2020-05-02 17:48:48 +0200

    Write basic scraper
    This is still missing proper logging and error management!

--- src/HfVault.fs
+++ src/HfVault.fs
@@ -26,6 +26,8 @@ open HtmlAgilityPack
 open HfVault
 open HfVault.Optics
 
+let userAgent = "hf-vault-bot/0.0.0 (+https://github.com/Aksamyt/hf-vault)"
+
 type Options = { realm : Realm.T
                } with
   static member new_ = { realm=Realm.FR
@@ -49,8 +51,112 @@ let rec parseArgv o = function
 | jaj::_ -> Error (jaj + ": unknown option")
 | []     -> Ok o
 
+let insertThread theme forumThread forumPosts = async
+{ match forumPosts with
+  | [||]  -> return ()
+  | posts ->
+      match!
+        posts.[0]
+        |> Forum.Post.makeNewHfUser
+        |> Db.``insert HfUser and Author or get id``
+      with
+      | None             -> return ()
+      | Some firstAuthor ->
+          let firstPostDate = posts.[0]^.Forum.Post.createdAt_ in
+          let lastPostDate = posts.[posts.Length - 1]^.Forum.Post.createdAt_ in
+          let thread =
+            Domain.Thread.new_
+            |> (firstAuthor^.Domain.HfUser.author_)^=Domain.Thread.author_
+            |> firstPostDate^=Domain.Thread.createdAt_
+            |> lastPostDate^=Domain.Thread.updatedAt_
+            |> theme^=Domain.Thread.theme_
+            |> (forumThread^.Forum.Thread.name_)^=Domain.Thread.name_
+            |> true^=Domain.Thread.open_
+            |> false^=Domain.Thread.sticky_
+          in
+          let hfThread =
+            Domain.HfThread.new_
+            |> (forumThread^.Forum.Thread.realm_)^=Domain.HfThread.realm_
+            |> (forumThread^.Forum.Thread.id_)^=Domain.HfThread.hfid_
+            |> thread^=Domain.HfThread.thread_
+          in
+          match! Db.``insert new HfThread and Thread`` hfThread with
+          | None             -> return ()
+          | Some hfThread ->
+              let threadId =
+                hfThread^.(Domain.HfThread.thread_ >-> Domain.Thread.id_)
+              in
+              for post in forumPosts do
+                match!
+                  post
+                  |> Forum.Post.makeNewHfUser
+                  |> Db.``insert HfUser and Author or get id``
+                with
+                | None        -> ()
+                | Some hfUser ->
+                    let domainPost =
+                      Domain.Post.new_
+                      |> (hfUser^.Domain.HfUser.author_)^=Domain.Post.author_
+                      |> (post^.Forum.Post.createdAt_)^=Domain.Post.createdAt_
+                      |> (post^.Forum.Post.content_)^=Domain.Post.message_
+                      |> threadId^=Domain.Post.thread_
+                    in
+                    let hfPost =
+                      Domain.HfPost.new_
+                      |> (post^.Forum.Post.id_)^=Domain.HfPost.hfid_
+                      |> (post^.Forum.Post.realm_)^=Domain.HfPost.realm_
+                      |> domainPost^=Domain.HfPost.post_
+                    in
+                    let! _ = Db.``insert new HfPost and Post`` hfPost in
+                    ()
+              done;
+              return ()
+}
+
+let insertTheme forumTheme =
+  let theme =
+    Domain.Theme.new_
+    |> (forumTheme^.Forum.Theme.name_)^=Domain.Theme.name_
+  in
+  let hfTheme =
+    Domain.HfTheme.new_
+    |> (forumTheme^.Forum.Theme.id_)^=Domain.HfTheme.hfid_
+    |> (forumTheme^.Forum.Theme.realm_)^=Domain.HfTheme.realm_
+    |> theme^=Domain.HfTheme.theme_
+  in
+  Db.insertHfThemeAndTheme hfTheme
+
+let ``scrape it!`` o = async
+{ let web = HtmlWeb(UserAgent="hf-vault") in
+  let themes =
+    match Forum.Root.load web o.realm with
+    | Some root -> root^.Forum.Root.themes_
+    | _         -> failwith "error loading forum root"
+  in
+  let work forumTheme = async
+  { match! insertTheme forumTheme with
+    | None         -> ()
+    | Some hfTheme ->
+        let theme = hfTheme^.Domain.HfTheme.theme_ in
+        let forumThreads = Forum.Theme.load web forumTheme in
+        for forumThread in forumThreads do
+          let forumPosts = Forum.Thread.load web forumThread |> List.toArray in
+          let! _ = insertThread theme forumThread forumPosts in
+          ()
+        done
+  } in
+  let! _ = themes.[0] |> work in
+  // let! _ = themes |> Array.map work |> Async.Parallel in
+  return ()
+}
+
 [<EntryPoint>]
 let main argv =
   match parseArgv Options.new_ (Array.toList argv) with
   | Error e -> Printf.eprintfn "error: %s" e; -1
-  | Ok o    -> 0
+  | Ok o    -> try ``scrape it!`` o |> Async.RunSynchronously; 0 with
+               | e -> Printf.eprintfn
+                        "fatal error: %s (%A)"
+                        e.Message
+                        e.TargetSite;
+                      -1
