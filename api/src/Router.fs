efd2e5cc37e4d686632effac5940c1ddadf7b948
Alexandre Szymocha <alexandre@szymocha.com>
2020-05-06 10:00:37 +0200

    Init project

---
+++ api/src/Router.fs
@@ -0,0 +1,29 @@
+(*
+    Copyright (C) 2020  Contributors as noted in the AUTHORS.md file
+    This file is part of hf-vault, an Eternal Twin preservation project.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*)
+
+module Api.Router
+#nowarn "62"
+#light "off"
+
+open Freya.Core
+open Freya.Routers.Uri.Template
+open Freya.Types.Http
+
+let root = freyaRouter
+{ ()
+}
